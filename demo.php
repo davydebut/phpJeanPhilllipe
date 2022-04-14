ae56b257984ebf130fa7ac1b2b853ebb2cc0ebcf
davydebut <spanudavy@gmail.com>
2022-04-14 14:13:56 +0200

    ajout html php

--- demo.php
+++ demo.php
@@ -1,10 +1,28 @@
 <?php
-
-// commebtaires en ligne
-/* 
-commentaires en ligne
-*/
-echo "bonjour"; // il affichera bonjour32
-echo 32;
-
+$message = 'bonjour';
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // commentaires en ligne
+    /* 
+        commentaires en ligne
+    */
+    echo $message; // il affichera bonjour32
+    // echo 32;
+
+    echo 32 + 8; // affichera 40
+
+    ?>
+</body>
+
+</html>
