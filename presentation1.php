e2ac498deee4c0741a5408b2eb7a8e1c2ba4dc89
davydebut <spanudavy@gmail.com>
2022-04-21 17:51:42 +0200

    Get

---
+++ presentation1.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Bonjour, votre prénom est <?php echo $_GET['prenom']?>, et votre nom est <?php echo $_GET['nom']?>. Vous avez <?php echo $_GET['age']?> ans.</p>
+</body>
+</html>
